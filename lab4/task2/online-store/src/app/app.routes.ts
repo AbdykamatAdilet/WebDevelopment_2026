9249c08eee62dcb59cd774f392ac7b9c792ab7e9
Adik <mcpeadilet@gmail.com>
2026-02-20 20:43:38 +0300

    Uploading fourth lab

---
+++ lab4/task2/online-store/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/product-list/product-list')
+        .then(m => m.ProductListComponent)
+  }
+];
